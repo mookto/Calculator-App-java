00215b278e8865c85bf25151355d5e3fdc47eff6
SADHIN <30hasanmahedi@gmail.com>
2025-08-08 22:00:37 +0600

    First commit

--- src/test/java/Calculator.java
+++ src/test/java/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator extends JFrame implements ActionListener {
 
     public Calculator() {
         setTitle("Java Calculator");
-        setSize(300, 400);
+        setSize(400, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null); // Center screen
         setLayout(new BorderLayout());
